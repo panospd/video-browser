c0c7168268b8614df98245e6703c5935b990642c
Panos <anastapd@gmail.com>
2020-04-30 16:56:51 +0100

    Fixed grid bug when no video was initially loaded videolist column was swifted to the right

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,11 +1,13 @@
 <template>
-  <div v-if="video" class="col-md-8">
-    <div class="embed-responsive embed-responsive-16by9">
-      <iframe class="embed-responsive-item" :src="videoUrl"></iframe>
-    </div>
-    <div class="details">
-      <h4>{{video.snippet.title}}</h4>
-      <p>{{video.snippet.description}}</p>
+  <div class="col-md-8">
+    <div v-if="video">
+      <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" :src="videoUrl"></iframe>
+      </div>
+      <div class="details">
+        <h4>{{video.snippet.title}}</h4>
+        <p>{{video.snippet.description}}</p>
+      </div>
     </div>
   </div>
 </template>
